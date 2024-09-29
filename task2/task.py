ad7489b7a63c7e3ed2db82f415be231c26edb677
Cicuovidiu <cicuovidiu1@gmail.com>
2024-09-29 21:04:26 +0300

    remove dockerfile

--- task2/task.py
+++ task2/task.py
@@ -1,15 +1,13 @@
 import requests
 import json
 
-# Simple Configuration: Define your API URL and endpoint
 API_URL = "https://wps-interview.azurewebsites.net"
 USERS_ENDPOINT = "/api/v1/user/"
 
 def get_users():
-    # Properly construct the complete URL for the API request
     response = requests.get(API_URL + USERS_ENDPOINT)
     if response.status_code == 200:
-        return response.json()  # Assuming the response is in JSON format
+        return response.json()  
     else:
         print(f"Error: Unable to fetch users (Status code: {response.status_code})")
         return []
@@ -20,10 +18,8 @@ def generate_email(user):
     lastname = user['lastname'].lower()
 
     if user.get('is_external'):
-        # External user
         return f"external_{lastname}.{firstname}@wps-allianz.de"
     else:
-        # Internal user
         return f"{firstname}.{lastname}@wps-allianz.de"
 
 def update_user_email(user_id, updated_user_data):
@@ -35,24 +31,20 @@ def update_user_email(user_id, updated_user_data):
     else:
         print(f"Failed to update user ID {user_id} (Status code: {response.status_code})")
 
-def filter_and_set_emails(users):
-    users_without_email = [user for user in users if not user.get('email')]
+def filter_and_set_emails(user_list):
+    users_without_email = [user for user in user_list  if not user.get('email')]
 
     # Process each user found without an email
     for user in users_without_email:
-        # Generate email
         generated_email = generate_email(user)
-        
-        # Update user data with the generated email
-        user['email'] = generated_email
 
-        # Update the user in the database
+        user['email'] = generated_email
+        
         update_user_email(user['id'], user)
 
     return users
 
 def save_to_json(users, filename="updated_users.json"):
-    # Save the updated users to a JSON file
     with open(filename, 'w') as f:
         json.dump(users, f, indent=4)
     print(f"Updated users saved to {filename}")
